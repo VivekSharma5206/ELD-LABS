dc924371f1b527e923e1cace30b307897be01ba0
Vivek Sharma <vivek24626@iiitd.ac.in>
2026-06-10 22:21:20 +0530

    Add files via upload

---
+++ LAB1/LAB1.srcs/sim_1/imports/Downloads/adder_tb.v
@@ -0,0 +1,17 @@
+module adder_tb();
+
+    reg [3:0] InA, InB;
+    wire [4:0] OutSum;
+    wire overflow;
+    top_adder tb0( .InA(InA), .InB(InB), .OutSum(OutSum), .overflow(overflow)
+    );
+    initial begin
+        InA = 4'b0000;     InB = 4'b0000;
+        #5 InA = 4'b0100 ; InB = 4'b0110;
+        #5 InA = 4'b0101 ; InB = 4'b0111;
+        #5 InA = 4'b0111 ; InB = 4'b0111;
+        #5 InA = 4'b1111 ; InB = 4'b1000;  //overflow
+        #5 InA = 4'b0111 ; InB = 4'b0001;
+        #5 InA = 4'b1111;  InB=4'b1111;  //overflow
+    end
+endmodule
